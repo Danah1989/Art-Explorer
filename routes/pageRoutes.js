79ba5c9737cb75c5d4434d179e9730cf2814c421
R <95617133+ReemaAlonazi@users.noreply.github.com>
2026-05-17 04:12:07 +0300

    Delete routes/pageRoutes.js

--- routes/pageRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const pageController = require('../controllers/pageController');
-
-// home page
-router.get('/', pageController.home);
-
-module.exports = router;
