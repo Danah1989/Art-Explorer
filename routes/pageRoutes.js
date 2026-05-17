3dfe7274d627ad8b3d2d4a0967c0cfbe9b070588
R <95617133+ReemaAlonazi@users.noreply.github.com>
2026-05-17 04:12:43 +0300

    Add files via upload

---
+++ routes/pageRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const pageController = require('../controllers/pageController');
+
+// home page
+router.get('/', pageController.home);
+
+module.exports = router;
